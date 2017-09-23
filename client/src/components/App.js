30166495ff60c17caab3345ac0f19eb1dda9f018
ryichang <ryichang@gmail.com>
2017-09-23 16:40:10 -0700

    Added componentDidMount for fetchUser in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-Class App extends Component {
+class App extends Component {
   componentDidMount() {
-    
+    this.props.fetchUser();
   }
 
   render() {
@@ -25,6 +27,6 @@ Class App extends Component {
       </div>
     );
   }
-};
+}
 
-export default App;
+export default connect(null, actions)(App);
