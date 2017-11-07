0e6293a056693ec447917ab475a7fc912b58c72a
ryichang <ryichang@gmail.com>
2017-11-06 21:10:32 -0800

    Post request for Stripe api handler in server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
-// 
+//
 // mongoose.connect(keys.mongoURI);
 mongoose.Promise = global.Promise
 mongoose.connect(keys.mongoURI, {
@@ -24,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
