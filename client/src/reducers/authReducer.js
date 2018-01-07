ad3282e59e0611e07ac94a95a3d3e53b4078c348
ryichang <ryichang@gmail.com>
2018-01-07 13:32:35 -0800

    Fixed page refresh

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,7 +3,11 @@ import { FETCH_USER } from '../actions/types';
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
+      console.log(action.payload)
+      console.log('state', state)
+
       return action.payload || false;
+      // return {...state, auth: action.payload || false}
     default:
       return state;
   }
