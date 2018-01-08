dffe64daa5fb99f16b24d15840b371c00ffa69b2
ryichang <ryichang@gmail.com>
2018-01-07 20:51:37 -0800

    Client side survey creation / material icons

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red">
+          <i className="material-icons">chat_bubble_outline</i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
