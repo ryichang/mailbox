5a4243803c4353dd23d31238a6d4d54be4e07af6
ryichang <ryichang@gmail.com>
2018-01-08 21:06:11 -0800

    Showing validation to SurveyField

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 //SurveyField contains logic to render a single label and text input
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   )
 };
