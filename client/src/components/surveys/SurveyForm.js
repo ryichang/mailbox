11e82aa7bedcdea674d55a6447ffb7fb8ce3e5f9
ryichang <ryichang@gmail.com>
2018-01-09 00:00:50 -0800

    Set back button in form review

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,37 +1,44 @@
 // SurveyForm shows a form for a user to add input
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-import SurveyField from './SurveyField';
-import { Link } from 'react-router-dom';
-import validateEmails from '../../utils/validateEmails';
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' },
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
 ];
-
-
 
 class SurveyForm extends Component {
   renderFields() {
     return _.map(FIELDS, ({ label, name }) => {
-      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
     });
   }
 
   render() {
-    return(
+    return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
-          <Link to="/surveys" className= "red btn-flat left white-text">
+          <Link to="/surveys" className="red btn-flat left white-text">
             Cancel
           </Link>
-          <button type="submit" className="teal btn-flat right white-text">Next
-          <i className="material-icons right">done</i>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
           </button>
         </form>
       </div>
@@ -42,12 +49,12 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  errors.emails = validateEmails(values.emails || '');
-  
+  errors.emails = validateEmails(values.emails || "");
+
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
-      errors[name] = 'You must provide a value';
-    };
+      errors[name] = "You must provide a value";
+    }
   });
 
   return errors;
@@ -55,5 +62,5 @@ function validate(values) {
 
 export default reduxForm({
   validate,
-  form: 'surveyForm'
+  form: "surveyForm"
 })(SurveyForm);
